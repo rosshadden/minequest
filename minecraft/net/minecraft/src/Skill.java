a37ba0c39c47c6ed064a451f1f2ea4f0b019c7f4
Ross Hadden <rosshadden@gmail.com>
2012-08-22 15:54:34 -0400

    Moved Sprint to its own skill file.

--- minecraft/net/minecraft/src/Skill.java
+++ minecraft/net/minecraft/src/Skill.java
@@ -1,16 +1,14 @@
 package net.minecraft.src;
 
-public class Skill{
-	private Item skill;
-
+public class Skill extends Item{
 	public Skill(int id, String name){
-		skill = new Item(id);
+		super(id);
 
-        skill.setTabToDisplayOn(CreativeTabs.tabCombat);
-		skill.setMaxStackSize(1);
-		skill.setIconCoord(10, 3);
-		skill.setItemName("skill" + name);
+        this.setTabToDisplayOn(CreativeTabs.tabCombat);
+		this.setMaxStackSize(1);
+		this.setIconCoord(10, 3);
+		this.setItemName("skill" + name);
 
-		ModLoader.addName(skill, name);
+		ModLoader.addName(this, name);
 	}
 }
