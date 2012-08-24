73074a26485304db589cd1da482e6aacd9c886da
Ross Hadden <rosshadden@gmail.com>
2012-08-24 16:37:17 -0400

    Awwwwww yeah.

--- minecraft/net/minecraft/src/BlockBookOfSkills.java
+++ minecraft/net/minecraft/src/BlockBookOfSkills.java
@@ -8,6 +8,10 @@ public class BlockBookOfSkills extends Block{
         this.setCreativeTab(CreativeTabs.tabDeco);
 	}
 
+	public int getBlockTextureFromSide(int i){
+		return i * 2 + 16;
+	}
+
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9){
 		ModLoader.openGUI(player, new GuiSkills(player));
 
