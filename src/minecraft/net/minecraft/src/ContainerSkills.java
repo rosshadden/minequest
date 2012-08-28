9549e1f5492f9805b3eb7fc1babbb24fb88fdeaf
Ross Hadden <rosshadden@gmail.com>
2012-08-28 18:27:38 -0400

    Sort-of-working slots for skillbar.

--- src/minecraft/net/minecraft/src/ContainerSkills.java
+++ src/minecraft/net/minecraft/src/ContainerSkills.java
@@ -5,64 +5,61 @@ public class ContainerSkills extends Container{
 
     public ContainerSkills(IInventory inventory, TileEntitySkills theTileEntitySkills){
         this.tileEntitySkills = theTileEntitySkills;
-        int var3;
-        int var4;
+        IInventory skillbar = new InventoryBasic("Skills", 9);
+        int i;
+        int j;
 
-        // for (var3 = 0; var3 < 3; ++var3){
-        //     for (var4 = 0; var4 < 3; ++var4){
-        //         this.addSlotToContainer(new Slot(theTileEntitySkills, var4 + var3 * 3, 62 + var4 * 18, 17 + var3 * 18));
-        //     }
-        // }
+        for (i = 0; i < 9; ++i){
+            this.addSlotToContainer(new Slot(skillbar, i, 8 + i * 18, 33));
+        }
 
-        for (var3 = 0; var3 < 3; ++var3){
-            for (var4 = 0; var4 < 9; ++var4){
-                this.addSlotToContainer(new Slot(inventory, var4 + var3 * 9 + 9, 8 + var4 * 18, 84 + var3 * 18));
+        for (i = 0; i < 3; ++i){
+            for (j = 0; j < 9; ++j){
+                this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
             }
         }
 
-        for (var3 = 0; var3 < 9; ++var3){
-            this.addSlotToContainer(new Slot(inventory, var3, 8 + var3 * 18, 142));
+        for (i = 0; i < 9; ++i){
+            this.addSlotToContainer(new Slot(inventory, i, 8 + i * 18, 142));
         }
     }
 
-    public boolean canInteractWith(EntityPlayer par1EntityPlayer){
-        return this.tileEntitySkills.isUseableByPlayer(par1EntityPlayer);
+    public boolean canInteractWith(EntityPlayer player){
+        return this.tileEntitySkills.isUseableByPlayer(player);
     }
 
     /**
      * Called to transfer a stack from one inventory to the other eg. when shift clicking.
      */
-    public ItemStack transferStackInSlot(int par1){
-        ItemStack var2 = null;
-        Slot var3 = (Slot)this.inventorySlots.get(par1);
+    public ItemStack transferStackInSlot(int slot){
+        ItemStack newItem = null;
+        Slot item = (Slot)this.inventorySlots.get(slot);
 
-        if (var3 != null && var3.getHasStack()){
-            ItemStack var4 = var3.getStack();
-            var2 = var4.copy();
+        if (item != null && item.getHasStack()){
+            ItemStack var4 = item.getStack();
+            newItem = var4.copy();
 
-            if (par1 < 9){
+            if (slot < 9){
                 if (!this.mergeItemStack(var4, 9, 45, true)){
                     return null;
                 }
-            }
-            else if (!this.mergeItemStack(var4, 0, 9, false)){
+            }else if (!this.mergeItemStack(var4, 0, 9, false)){
                 return null;
             }
 
             if (var4.stackSize == 0){
-                var3.putStack((ItemStack)null);
-            }
-            else{
-                var3.onSlotChanged();
+                item.putStack((ItemStack)null);
+            }else{
+                item.onSlotChanged();
             }
 
-            if (var4.stackSize == var2.stackSize){
+            if (var4.stackSize == newItem.stackSize){
                 return null;
             }
 
-            var3.onPickupFromSlot(var4);
+            item.onPickupFromSlot(var4);
         }
 
-        return var2;
+        return newItem;
     }
 }
